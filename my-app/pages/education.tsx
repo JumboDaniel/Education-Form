770f4f38b3200e4142bb17c3046e8472ef68b980
Jumbo Daniel <jumbodaniel15@gmail.com>
2022-07-09 00:03:58 +0800

    Add Typescript to the project

---
+++ my-app/pages/education.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react'
+
+const Education = () => {
+  return (
+    <div className="user-container">
+      <h1>Type your name and click "Enter" to begin</h1>
+      <input />
+      <button>Enter</button>
+    </div>
+  );
+};
+
+export default Education;
